65fdd54631d6729f2a3eee1cbc0471ea5bde3735
AAPoke <56592511+aapokepikachu@users.noreply.github.com>
2025-02-11 22:22:34 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+TOKEN = "YOUR_BOT_TOKEN"  # Replace with your bot's API token
+GROUP_A_ID = -123456789   # Admin group ID
+GROUP_P_ID = -987654321   # Private group ID
+ADMIN_IDS = [111111111, 222222222]  # List of bot admin user IDs
